9c521f3a152d6977390ca91905ae527895b80794
Elaiza Endrinal <endrinalkyla8@gmail.com>
2024-01-24 23:58:27 +0800

    Pseudocode

---
+++ Base_and_Exponent.py
@@ -0,0 +1,9 @@
+# Write a function called exponent(base, exp) that returns an int value of base raises to the power of exp.
+
+# pseudocode
+
+# Define the function of exponent
+
+# Testing Case 1
+
+# Testing Case 2
