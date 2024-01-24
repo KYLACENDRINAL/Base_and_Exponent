30049073339b335f61fbfc87d0048de20a23a2bd
Elaiza Endrinal <endrinalkyla8@gmail.com>
2024-01-25 00:04:28 +0800

    Tested Case 1

--- Base_and_Exponent.py
+++ Base_and_Exponent.py
@@ -10,5 +10,10 @@ def exponent(base, exp):
     return result
 
 # Testing Case 1
+print("Case 1: ")
+base1=2
+exp1=5
+result1=exponent(base1, exp1)
+print(f"{base1} raises to the power of {exp1} is {result1}")
 
 # Testing Case 2
